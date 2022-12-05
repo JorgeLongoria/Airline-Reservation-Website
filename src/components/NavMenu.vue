613b7f5a1ce363cf211395b99ecd33c08b812dbd
Jorge Longoria <jlongo48@stu.southtexascollege.edu>
2022-12-04 18:59:58 -0600

    Added design to home page

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,18 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+const title = ref(import.meta.env.VITE_APP_NAME)
+</script>
+
 <template>
   <nav>
-    <RouterLink to="/Home">Home</RouterLink>
-    <RouterLink to="/BookFlight">Book Flight</RouterLink>
-    <RouterLink to="/Destinations">Destinations</RouterLink>
-    <RouterLink to="/Login">Login</RouterLink>
+    <div class="wrapper">
+      <RouterLink to="/Home" class="brand">
+        <span class="brand-title">{{ title }}</span>
+      </RouterLink>
+      <div class="menu">
+      <RouterLink to="/BookFlight" href="#" class="menu-items">Book Flight</RouterLink>
+      <RouterLink to="/Destinations" href="#" class="menu-items">Destinations</RouterLink>
+      <RouterLink to="/Login" href="#" class="menu-login">Login</RouterLink>
+      </div>
+    </div>
   </nav>
 </template>
 
 <style lang="postcss" scoped>
-nav {
-  @apply flex justify-center space-x-4;
+  nav {
+    @apply h-20 bg-yellow-900;
+    .wrapper {
+      @apply container mx-auto flex w-full items-center justify-between;
+      .brand {
+        &-title {
+          @apply text-4xl font-semibold text-white;
+        }
+      }
+      .menu {
+        @apply flex gap-2;
 
-  & .router-link-active {
-    @apply underline underline-offset-4;
+        &-items {
+          @apply mt-5 text-white hover:text-gray-600;
+        }
+        &-login {
+          @apply mt-5 text-white hover:text-green-300;
+        }
+      }
+    }
   }
-}
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,7 +4,38 @@ const title = ref('Home Page')
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin">{{ title }}</h1>
-  </main>
+    <div class="bg-container">
+      <img class="background-img" src="https://static.toiimg.com/photo/70739792.cms" >
+      <div class="text_top">Fast, Frequent and Direct Flights</div>
+      <RouterLink to="/BookFlight" class="book">
+        <h1 class="book-link"> Book Now! </h1>
+      </RouterLink>
+    </div>
 </template>
+
+<style scoped lang="postcss">
+  .bg-container {
+    background-image: url("https://static.toiimg.com/photo/70739792.cms");
+    position: relative;
+    text-align: center;
+    color: black;
+  }
+
+  .text_top {
+    position: absolute;
+    top: 10%;
+    left: 25%;
+    font-size: 50px;
+  }
+
+  .book-link {
+    position: absolute;
+    top: 20%;
+    left: 39%;
+    font-size: 50px;
+  }
+
+  .book-link {
+    @apply mx-1 rounded-md bg-yellow-900 px-4 py-2 text-black hover:bg-yellow-800;
+  }
+</style>
