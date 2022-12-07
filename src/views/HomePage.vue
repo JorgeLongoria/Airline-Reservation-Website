fedb2a64b86489b8781dcc315d3644c5ca105343
Jorge Longoria <jlongo48@stu.southtexascollege.edu>
2022-12-06 20:32:06 -0600

    Added home paragraph

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,7 +5,12 @@ import { ref } from 'vue'
 <template>
     <div class="bg-container">
       <img class="background-img" src="https://static.toiimg.com/photo/70739792.cms" >
-      <div class="text_top">Fast, Frequent and Direct Flights</div>
+      <div class="text_top">Fast, Frequent and Direct Flights
+        <p class="paragraph">After flying domestic flights for years, our 
+          airline offers a professional and low cost travel service. Whatever destination you chose,
+          our airplanes will get you there!
+        </p>
+      </div>
       <RouterLink to="/BookFlight" class="book">
         <h1 class="book-link"> Book Now! </h1>
       </RouterLink>
@@ -23,18 +28,22 @@ import { ref } from 'vue'
   .text_top {
     position: absolute;
     top: 10%;
-    left: 25%;
+    left: 3%;
     font-size: 50px;
+  }
+
+  .paragraph {
+    font-size: 30px;
   }
 
   .book-link {
     position: absolute;
-    top: 20%;
+    top: 30%;
     left: 39%;
     font-size: 50px;
   }
 
   .book-link {
-    @apply mx-1 rounded-md bg-yellow-900 px-4 py-2 text-black hover:bg-yellow-800;
+    @apply mx-1 rounded-md bg-yellow-900 px-2 py-1 text-black hover:bg-yellow-800;
   }
 </style>
